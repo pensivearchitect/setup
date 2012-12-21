9829bd8e707008d2d1584ec61066686731a1db3b
Joshua <pensivearchitect@gmail.com>
2012-12-21 13:31:56 -0800

    added instructions to fix time machine when volume verification fails.

---
+++ brewsetup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+brew tap homebrew/dupes
+
+brew install node mongodb redis ruby s-lang snort v8 wget wkhtmltopdf zeromq coreutils dpkg fortune aspell autoconf autojump casperjs gnu-sed gnu-tar mercurial nmap zsh sqlite urlview mutt notmuch offline-imap openssl diction daq cmake contacts msmtp nagios scons readline re2 mit-scheme llvm gcc 
---
+++ debianinstall.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+
+deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen
+
+wget -O node.tar.gz
+
+sudo apt-get update
+
+sudo apt-get upgrade
+
+sudo apt-get install vim apparmor apparmor-utils gcc g++ git ksplice git mongodb20-10gen build-essential openssl libssl-cev upstart libtool autoconf automake uuid-dev python make
+
+mkdir ~/nodejs && cd $_
+
+wget -N http://nodejs.org/dist/node-latest.tar.gz
+
+tar xzvf node-latest.tar.gz && cd `ls -rd node-v*`
+
+./configure 
+
+make install -j4
+
+mkdir /opt/git/vimconfig
+
+cd /opt/git/vimconfig
+
+git init
+
+cd ~
+
+mkdir code
+
+cd code
+
+git --bare init
+
+npm update
+
+npm install express formidable mongoose pcap useragent parsley grunt node-uuid
+
+grunt init:node
+
+grunt watch
